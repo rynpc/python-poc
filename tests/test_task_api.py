c4b64268d75efb52400d2e38067cc0fb756817f4
rynpc <nomomtvisions@gmail.com>
2025-06-22 00:57:01 -0500

    Update test_task_api.py

--- tests/test_task_api.py
+++ tests/test_task_api.py
@@ -1,12 +1,7 @@
 import unittest
 from datetime import datetime
 
-from src.task_api import (
-    add_task_api,
-    delete_task_api,
-    list_tasks_api,
-    mark_completed_api,
-)
+from src.task_api import (add_task_api, delete_task_api, list_tasks_api, mark_completed_api)
 from src.task_manager import TaskManager
 
 
